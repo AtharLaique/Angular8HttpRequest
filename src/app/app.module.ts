aef76897ca5f62452102931962c22471c6456791
AtharLaique <37653074+AtharLaique@users.noreply.github.com>
2020-02-15 17:31:37 +0500

    Enable http Module in project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+//import HttpClientModule here
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
+  //Add in imports array
   imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
