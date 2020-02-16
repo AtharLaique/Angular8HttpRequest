f0044ea4e3a18fcc0b4f3376699ef98ff9dcbddc
AtharLaique <37653074+AtharLaique@users.noreply.github.com>
2020-02-16 18:45:02 +0500

    TYPE CASTING of HTTP REQUEST STEP 1 :[ import model to use it]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 
+import { Post } from './post.model';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
